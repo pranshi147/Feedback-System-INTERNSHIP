6dd129d4294a93ae0756ad5d39db436a899da6a7
pranshi147 <pranshi147btcse25@igdtuw.ac.in>
2026-07-02 17:50:05 +0530

    Configuration done

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
+from src.core.config import settings
 import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
