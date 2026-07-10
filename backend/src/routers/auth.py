25f051061c1cc08f7e9f60964914eab98bf7d7f8
pranshi147 <pranshi147btcse25@igdtuw.ac.in>
2026-07-10 19:53:08 +0530

    Set up authentication router and login request schema

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -1,9 +1,6 @@
 from fastapi import APIRouter
 
-
-
-router= APIRouter(prefix ="/auth", tags=["Authentication"])
-
-@router.get("/")
-def test():
-    return {"message": "Auth router working"}
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
