62f18b8dec4df2a281cb6a6c20df10affa34ea11
pranshi147 <pranshi147btcse25@igdtuw.ac.in>
2026-07-10 19:50:59 +0530

    Implement authentication utilities with password hashing and JWT

---
+++ backend/src/core/security.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from src.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hashing(password: str):
+    return pwd_context.hash(password)
+
+def verification(plain_pass: str, hashed: str):
+    return pwd_context.verify(plain_pass, hashed)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode.update({"exp": expire})
+    return jwt.encode(
+            to_encode,
+            settings.SECRET_KEY,
+            algorithm=settings.ALGORITHM
+        )
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+
+        return payload
+
+    except JWTError:
+        return None
