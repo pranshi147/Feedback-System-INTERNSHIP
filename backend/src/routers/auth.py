7c5cdd52de870cc578401c86e40e058a1be7bedd
pranshi147 <pranshi147btcse25@igdtuw.ac.in>
2026-07-10 22:19:39 +0530

    Implement user login with JWT authentication

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -1,6 +1,48 @@
 from fastapi import APIRouter
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from src.database import get_db
+from src.schemas.login import LoginRequest
+from src.services.user_service import get_user_by_email
+from src.core.security import (
+    verify_password,
+    create_access_token
+)
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
 )
+
+@router.post("/login")
+def login(
+    request: LoginRequest,
+    db: Session = Depends(get_db)
+):
+    user = get_user_by_email(
+    db,
+    request.email
+)
+    if not user:
+        raise HTTPException(
+        status_code=401,
+        detail="Invalid email or password"
+    )
+    if not verify_password(
+    request.password,
+    user.password
+):
+        raise HTTPException(
+    status_code=401,
+    detail="Invalid email or password"
+)
+    token = create_access_token(
+    {
+        "sub": user.email
+    }
+)
+    return {
+    "access_token": token,
+    "token_type": "bearer"
+}
