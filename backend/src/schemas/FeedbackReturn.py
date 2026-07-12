72ccffd30632ea4adcf0eacaf400e366d783ff2f
pranshi147 <pranshi147btcse25@igdtuw.ac.in>
2026-07-13 00:44:54 +0530

    Refactor project structure and remove unused schemas

--- backend/src/schemas/FeedbackReturn.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import String, ARRAY, Integer
-from sqlalchemy.orm import Mapped, mapped_column
-
-class FeedbackReturn:
-    uuid: Mapped[int] = mapped_column(primary_key=True)
-    questions: Mapped[list[int]] = mapped_column(ARRAY(Integer))
-    commentBox: Mapped[str] = mapped_column(String(1023))
